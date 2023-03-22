8f9390326ca3b702e0b7da6a2309f7b1702acb1a
zeyujiang8800 <114033081+zeyujiang8800@users.noreply.github.com>
2023-03-22 14:50:34 -0400

    Update bubblesort.c

--- bubblesort.c
+++ bubblesort.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
-int bubble_sort (int *array, int num) {
-    int temp, count = 0;
-    for (int i = 0; i < num; i++) {
+void bubble_sort (int *array, int num) {
+    int temp, count;
+    for (int i = 0; i < num-1; i++) {
+        count = 0;
         for (int j = 0; j < num-1-i; j++) {
             if (array[j] > array[j+1]) {
                 temp = array[j];
@@ -11,15 +12,15 @@ int bubble_sort (int *array, int num) {
                 count++;
             }
         }
+        printf("Iteration #%d: %d\n", i+1, count);
     }
-    return count;
 }
 
 int main () {
     int array[9] = {97, 16, 45, 63, 13, 22, 7, 58, 72};
     int swaps;
-    swaps = bubble_sort(array, 9);
-    printf("Number of swaps: %d\n", swaps);
+    bubble_sort(array, 9);
+    printf("\nThe sorted array: ");
     for (int i = 0; i < 9; i++) {
         printf("%d ", array[i]);
     }
